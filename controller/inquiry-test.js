aaf7f57475f08358d307ec076250034d1fba20b6
path71nder <sevillebayuaji@gmail.com>
2023-03-25 10:08:52 +0700

    Expired date added

--- controller/inquiry-test.js
+++ controller/inquiry-test.js
@@ -6,10 +6,11 @@ const InquiryTest = function(inquiryTest){
     this.amount = inquiryTest.amount;
     this.cust_name = inquiryTest.cust_name;
     this.signature = inquiryTest.signature;
+    this.expired_date = inquiryTest.expired_date;
 }
 
 InquiryTest.findByIdSignature = (id, result) => {
-    sqlPoolProd.query(`SELECT va_number, amount, cust_name FROM payment_test WHERE va_number = ${id}`, (err, res) => {
+    sqlPoolProd.query(`SELECT va_number, amount, cust_name, expired_date FROM payment_test WHERE va_number = ?`, [id] , (err, res) => {
         if(err){
             console.log(err);
             result(err, null);
--- middleware/inquiry-middleware-test.js
+++ middleware/inquiry-middleware-test.js
@@ -6,6 +6,12 @@ exports.getAmountSignatureById = (req, res) => {
     const type = req.query.type;
     const trx_uid = req.query.trx_uid;
     const amount = req.query.amount;
+    const {
+        request, merchant_id, merchant, bill_no, bill_reff, bill_date, bill_expired, bill_desc, bill_currency, bill_gross, bill_miscfee, bill_total,
+        cust_no, cust_name, payment_channel, pay_type, bank_userid, msisdn, email, terminal, billing_name, billing_lastname, billing_address,
+        billing_address_city, billing_address_region, billing_address_state, billing_address_poscode, billing_msisdn, billing_address_country_code,
+        receiver_name_for_shipping, shipping_lastname, shipping_address, shipping_address_city, shipping_address_region, shipping_address_state,
+        shipping_address_poscode, shipping_msisdn, shipping_address_country_code, item, reserve1, reserve2 } = req.body;
 
     InquiryTest.findByIdSignature(id, (err, data) => {
         if(err){
@@ -15,6 +21,14 @@ exports.getAmountSignatureById = (req, res) => {
                 res.status(500).send({ message: `Failed to retrieve the data from ${id}`, response_code: '01' });
             }
         } else {
+            const currentDate = new Date();
+            const expiredDate = new Date(data.expired_date);
+
+            if(currentDate > expiredDate){
+                res.status(403).send({ message: 'Expired Virtual Account!', response_code: '01'});
+                return;
+            }
+
             const inquiryData = {
                 response: "VA Static Response",
                 ...data,
